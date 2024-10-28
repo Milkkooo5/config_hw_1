0de3bca0fffb34b7d4c10c922f01f7f81a7cd34e
Milana <ananbekh@icloud.com>
2024-10-28 17:31:24 +0300

    Прописываем функцию main

--- shell.py
+++ shell.py
@@ -2,3 +2,13 @@ import os
 import tarfile
 import argparse
 from datetime import datetime
+
+def main():
+    parser = argparse.ArgumentParser(description="Shell Emulator")
+    parser.add_argument('--user', required=True, help="Username for shell prompt")
+    parser.add_argument('--vfs', required=True, help="Path to the virtual file system (tar archive)")
+
+    args = parser.parse_args()
+
+if __name__ == "__main__":
+    main()
