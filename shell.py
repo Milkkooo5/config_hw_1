02eec68071eb8d1d00bb7d7954deb7e8ab7e7ea7
Milana <ananbekh@icloud.com>
2024-10-30 07:13:36 +0300

    Добавляем методы run и execute_command

--- shell.py
+++ shell.py
@@ -9,6 +9,35 @@ class ShellEmulator:
         self.vfs = vfs
         self.history = []
 
+    def run(self):
+        while True:
+            command_input = input(f"{self.username}@virtual:{self.vfs.current_dir}$ ")
+            self.history.append(command_input)
+            self.execute_command(command_input)
+
+    def execute_command(self, command):
+        parts = command.split()
+        if not parts:
+            return
+        cmd = parts[0]
+
+        if cmd == "ls":
+            self.ls()
+        elif cmd == "cd":
+            if len(parts) > 1:
+                self.cd(parts[1])
+            else:
+                print("cd: missing operand")
+        elif cmd == "exit":
+            print("Exiting shell.")
+            exit(0)
+        elif cmd == "history":
+            self.history_cmd()
+        elif cmd == "wc":
+            self.wc(parts[1] if len(parts) > 1 else None)
+        else:
+            print(f"{cmd}: command not found")
+
 
 def main():
     parser = argparse.ArgumentParser(description="Shell Emulator")
